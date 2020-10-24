bebcd7986aadf84e3d31f973d6b807adca8ef597
nicholasrebstock <54780738+nicholasrebstock@users.noreply.github.com>
2020-10-24 12:40:46 -0600

    add comments and fix string values in string.xml

--- app/src/main/java/com/example/bookit/MainActivity.java
+++ app/src/main/java/com/example/bookit/MainActivity.java
@@ -16,10 +16,14 @@ import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    // initialize variables to track which tab is selected in each mode, and which mode is active
+    // (borrower or lender). Default is borrower.
     boolean borrower = true;
     int currentId;
     int borrowerId;
     int lenderId = R.id.MyBooksFragment;
+
+    // declare variables for nav views
     BottomNavigationView botNavViewBorrower;
     BottomNavigationView botNavViewLender;
     @Override
@@ -28,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // assign value to navViews and set listeners so that whenever an item is selected, the
+        // fragment with the id of the item selected is opened.
+        // Whenever any any selection is made, set currentId to that id. CurrentId Will always hold
+        // id of last "tab" selected.
         botNavViewBorrower = findViewById(R.id.navigationViewBorrower);
         botNavViewBorrower.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -63,12 +72,19 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
+
+        // if profile button is clicked, open ProfileActivity
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, ProfileActivity.class);
             startActivity(intent);
             return true;
-        } else if (id == R.id.menu_toggle) {
+        }
+        // else if other button is clicked, change mode from borrower to lender or vice versa, then
+        // save the id of the opposite mode to the last accessed "tab". Switch the visibility of
+        // each navBar to correspond with switching modes. Restore currentId to the saved value for
+        // the other mode, and switch to whichever fragment was active by switching to saved value
+        // (i.e. lenderId or borrowerId).
+        else if (id == R.id.menu_toggle) {
             borrower = !borrower;
             if (borrower) {
                 lenderId = currentId;
