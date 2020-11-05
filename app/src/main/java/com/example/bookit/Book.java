0bfb7508ca576d43635af2e5e79452d27dbf91f6
Vyome Agarwal <vyome@ualberta.ca>
2020-11-04 17:45:54 -0700

    added Javadoc for Book class

--- app/src/main/java/com/example/bookit/Book.java
+++ app/src/main/java/com/example/bookit/Book.java
@@ -5,6 +5,7 @@ public class Book {
     private String author;
     private String ISBN;
     private String status;
+    private String description;
     private String borrower = "N/A";
 
     public Book(String bookTitle, String author, String ISBN, String status) {
@@ -22,45 +23,120 @@ public class Book {
         this.borrower = borrower;
     }
 
+    public Book(String bookTitle, String author, String ISBN, String status, String description, String borrower) {
+        this.bookTitle = bookTitle;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.status = status;
+        this.description = description;
+        this.borrower = borrower;
+    }
+
+    /**
+     * Sets the bookTitle. This is the name of the Book
+     *
+     * @param bookTitle Book name
+     */
     public void setBookTitle(String bookTitle) {
         this.bookTitle = bookTitle;
     }
 
+    /**
+     * Sets the Author name for the Book
+     *
+     * @param author Author name
+     */
     public void setAuthor(String author) {
         this.author = author;
     }
 
+    /**
+     * Sets the ISBN of the Book
+     *
+     * @param ISBN ISBN of the Book
+     */
     public void setISBN(String ISBN) {
         this.ISBN = ISBN;
     }
 
+    /**
+     * Sets the status of the Book
+     *
+     * @param status status of the Book
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * Sets the description of the Book
+     *
+     * @param description description of the Book
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Sets the borrower of the Book (if any)
+     *
+     * @param borrower The name/userid of the borrower
+     */
     public void setBorrower(String borrower) {
         this.borrower = borrower;
     }
 
+    /**
+     * Returns the name of the Book
+     *
+     * @return the name of the Book
+     */
     public String getBookTitle() {
         return bookTitle;
     }
 
+    /**
+     * Returns the name of the Author
+     *
+     * @return the name of the Author
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * Returns the ISBN of the Book
+     *
+     * @return the ISBN of the Book
+     */
     public String getISBN() {
         return ISBN;
     }
 
+    /**
+     * Returns the Status of the Book
+     *
+     * @return the status of the Book
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Returns the description of the Book
+     *
+     * @return
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Returns the borrower name (if any)
+     *
+     * @return the borrower name (if any)
+     */
     public String getBorrower() {
         return borrower;
     }
-
-
 }
