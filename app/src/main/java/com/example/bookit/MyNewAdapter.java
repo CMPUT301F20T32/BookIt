b351dcbc9aba8c1d546c6eb659e62f642938c0a4
Minh Luu <nluu@ualberta.ca>
2020-11-05 21:41:40 +0700

    New Adapter

--- app/src/main/java/com/example/bookit/MyNewAdapter.java
+++ app/src/main/java/com/example/bookit/MyNewAdapter.java
@@ -1,4 +1,91 @@
 package com.example.bookit;
 
-public class MyNewAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyNewAdapter extends RecyclerView.Adapter<MyNewAdapter.MyViewHolder> {
+    private ArrayList<Book> mDataset;
+
+    private RecyclerViewClickListener mListener;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        // each data item is just a string in this case
+        public ImageView mImageView;
+        public TextView mBookTitle;
+        public TextView mAuthor;
+        public TextView mISBN;
+        public TextView mStatus;
+        public TextView mBorrower;
+
+        private RecyclerViewClickListener mListener;
+
+        public MyViewHolder(View v, RecyclerViewClickListener listener) {
+            super(v);
+            //mImageView = v.findViewById(R.id.book_image);
+            mBookTitle = v.findViewById(R.id.book_title);
+            mAuthor = v.findViewById(R.id.author);
+            mISBN = v.findViewById(R.id.isbn);
+            mStatus = v.findViewById(R.id.status);
+            mBorrower = v.findViewById(R.id.borrower);
+
+            mListener = listener;
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyNewAdapter(ArrayList<Book> myDataset, RecyclerViewClickListener listener) {
+        mDataset = myDataset;
+        mListener = listener;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @NonNull
+    @Override
+    public MyNewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.book_item_layout, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v, mListener);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manger)
+    @Override
+    public void onBindViewHolder(@NonNull MyNewAdapter.MyViewHolder holder, int position) {
+        // -get element from your dataset at this position
+        // -replace the contents of the view with that element
+        Book currentItem = mDataset.get(position);
+        //holder.mImageView.setImageResource(currentItem.getImag);
+        holder.mBookTitle.setText(mDataset.get(position).getBookTitle());
+        holder.mAuthor.setText(mDataset.get(position).getAuthor());
+        holder.mISBN.setText(mDataset.get(position).getISBN());
+        holder.mStatus.setText(mDataset.get(position).getStatus());
+        holder.mBorrower.setText(mDataset.get(position).getBorrower());
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
 }
