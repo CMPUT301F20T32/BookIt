3362eaa4ad84af2b01deb59e01299ff2f894846a
Vyome Agarwal <vyome@ualberta.ca>
2020-11-04 17:10:22 -0700

    borrower can view their borrowed books, username and owner name left to do

--- app/src/main/java/com/example/bookit/BorrowedFragment.java
+++ app/src/main/java/com/example/bookit/BorrowedFragment.java
@@ -1,15 +1,30 @@
 package com.example.bookit;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class BorrowedFragment extends Fragment {
+    private RecyclerView borrowedBorrowerRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     public View onCreateView(
@@ -30,8 +45,63 @@ public class BorrowedFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_borrowed, container, false);
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        borrowedBorrowerRecyclerView = view.findViewById(R.id.borrowed_borrower_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        borrowedBorrowerRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(view.getContext());
+        borrowedBorrowerRecyclerView.setLayoutManager(layoutManager);
+        ArrayList<Book> myDataset = new ArrayList<Book>();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("users2").document("eJl7kfYl5eRlNIs44Aqt");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("READ_DATA", "DocumentSnapshot Data: " + document.getData());
+                        ArrayList<String> bookIDs = (ArrayList<String>) document.get("borrowed_books");
+
+                        for (String bookID : bookIDs) {
+                            DocumentReference docRef2 = db.collection("books").document(bookID);
+                            docRef2.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        DocumentSnapshot document2 = task.getResult();
+                                        if (document2.exists()) {
+                                            Log.d("READ_BOOKS", "DocumentSnapshot data: " + document2.getData());
+                                            myDataset.add(new Book(document2.get("book_title").toString(), document2.get("author").toString(), document2.get("isbn").toString(), document2.get("status").toString()));
+                                            mAdapter.notifyDataSetChanged();
+
+                                        } else {
+                                            Log.d("READ_BOOKS", "No such document");
+                                        }
+                                    } else {
+                                        Log.d("READ_BOOKS", "get failed with ", task.getException());
+                                    }
+                                }
+                            });
+                        }
+                    } else {
+                        Log.d("READ_DATA", "No such document");
+                    }
+                } else {
+                    Log.d("READ_DATA", "get failed with ", task.getException());
+                }
+            }
+        });
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        borrowedBorrowerRecyclerView.setAdapter(mAdapter);
 
     }
 
