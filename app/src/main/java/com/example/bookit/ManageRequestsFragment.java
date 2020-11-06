4568e1bcdf75359862f8073dfb97f416366c398d
alisha03 <crasta@ualberta.ca>
2020-11-06 18:29:08 +0530

    Updated manage requests to get current user data

--- app/src/main/java/com/example/bookit/ManageRequestsFragment.java
+++ app/src/main/java/com/example/bookit/ManageRequestsFragment.java
@@ -20,6 +20,8 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.SuccessContinuation;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -115,8 +117,9 @@ public class ManageRequestsFragment extends Fragment {
         ArrayList<Book> myDataset = new ArrayList<Book>();
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        //TODO get current user data
-        DocumentReference docRef = db.collection("users2").document("eJl7kfYl5eRlNIs44Aqt");
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        DocumentReference docRef = db.collection("users2").document(currentUser.getEmail());
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
@@ -125,38 +128,39 @@ public class ManageRequestsFragment extends Fragment {
                     if (document.exists()) {
                         Log.d("READ_DATA", "DocumentSnapshot Data: " + document.getData());
                         HashMap<Object, String> bookIDs = (HashMap<Object, String>) document.get("my_books");
+                        if(!bookIDs.entrySet().isEmpty()){
+                            for (Map.Entry mapElement : bookIDs.entrySet()) {
+                                String key = (String) mapElement.getKey();
+                                String value = (String) mapElement.getValue();
 
-                        for (Map.Entry mapElement : bookIDs.entrySet()) {
-                            String key = (String) mapElement.getKey();
-                            String value = (String) mapElement.getValue();
+                                if (value.equals("requested")) {
+                                    DocumentReference docRef2 = db.collection("books").document(key);
+                                    docRef2.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                DocumentSnapshot document2 = task.getResult();
+                                                if (document2.exists()) {
+                                                    Log.d("READ_BOOKS", "DocumentSnapshot data: " + document2.getData());
 
-                            if (value.equals("requested")) {
-                                DocumentReference docRef2 = db.collection("books").document(key);
-                                docRef2.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                        if (task.isSuccessful()) {
-                                            DocumentSnapshot document2 = task.getResult();
-                                            if (document2.exists()) {
-                                                Log.d("READ_BOOKS", "DocumentSnapshot data: " + document2.getData());
+                                                    String bookTitle = document2.get("book_title").toString();
 
-                                                String bookTitle = document2.get("book_title").toString();
+                                                    List<String> requesters = (List<String>) document2.get("requesters");
+                                                    for (int i = 0; i < requesters.size(); i++) {
+                                                        myDataset.add(new Book(bookTitle, requesters.get(i), document2.get("owner").toString()));
+                                                    }
 
-                                                List<String> requesters = (List<String>) document2.get("requesters");
-                                                for (int i = 0; i < requesters.size(); i++) {
-                                                    myDataset.add(new Book(bookTitle, requesters.get(i), document2.get("owner").toString()));
+                                                    mAdapter.notifyDataSetChanged();
+
+                                                } else {
+                                                    Log.d("READ_BOOKS", "No such document");
                                                 }
-
-                                                mAdapter.notifyDataSetChanged();
-
                                             } else {
-                                                Log.d("READ_BOOKS", "No such document");
-                                            }
-                                        } else {
                                             Log.d("READ_BOOKS", "get failed with ", task.getException());
+                                            }
                                         }
-                                    }
-                                });
+                                    });
+                                }
                             }
                         }
                     } else {
@@ -191,7 +195,7 @@ public class ManageRequestsFragment extends Fragment {
                             .get()
                             .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                                 @Override
-                                public  void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
                                     if (task.isSuccessful()) {
                                         DocumentSnapshot doc = task.getResult().getDocuments().get(0);
                                         if (doc.exists()) {
@@ -200,31 +204,14 @@ public class ManageRequestsFragment extends Fragment {
                                             db.collection("books").document(bookID).update("requesters", FieldValue.arrayRemove(clickedBook.getRequester()));
                                             ArrayList<String> requesters = (ArrayList<String>) doc.get("requesters");
                                             if (requesters.size() == 1) {
-                                                if (requesters.get(0).equals(clickedBook.getRequester())){
+                                                if (requesters.get(0).equals(clickedBook.getRequester())) {
                                                     db.collection("books").document(bookID).update("status", "available");
-                                                    //query for user email
-                                                    db.collection("users2").whereEqualTo("user_info.username", clickedBook.getOwner())
-                                                            .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                                                @Override
-                                                                public  void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                                                    if (task.isSuccessful() & task.getResult().size() > 0) {
-                                                                        DocumentSnapshot doc = task.getResult().getDocuments().get(0);
-                                                                        Log.d("READ_DATA", "DocumentSnapshot Data: " + doc.getData());
-                                                                        if (doc.exists()) {
-                                                                            db.collection("users2").document(doc.getId()).update("my_books." + bookID, "available");
-                                                                            myDataset.remove(clickedBook);
-                                                                            mAdapter.notifyDataSetChanged();
-                                                                            Toast.makeText(getContext(), "Request by: " + clickedBook.getRequester() + "for: " + clickedBook.getBookTitle() + "declines", Toast.LENGTH_SHORT).show();
-                                                                        } else {
-                                                                            Log.d("READ_DATA", "No such document");
-                                                                        }
-                                                                    } else {
-                                                                        Log.d("QUERY_DATA", "query failed with ", task.getException());
-                                                                    }
-                                                                }});
+                                                    docRef.update("my_books." + bookID, "available");
+                                                    myDataset.remove(clickedBook);
+                                                    mAdapter.notifyDataSetChanged();
+                                                    Toast.makeText(getContext(), "Request by: " + clickedBook.getRequester() + "for: " + clickedBook.getBookTitle() + "declines", Toast.LENGTH_SHORT).show();
                                                 }
-                                            }
-                                            else {
+                                            } else {
                                                 myDataset.remove(clickedBook);
                                                 mAdapter.notifyDataSetChanged();
                                                 Toast.makeText(getContext(), "Request by: " + clickedBook.getRequester() + " for: " + clickedBook.getBookTitle() + " declined", Toast.LENGTH_SHORT).show();
@@ -232,11 +219,11 @@ public class ManageRequestsFragment extends Fragment {
                                         } else {
                                             Log.d("READ_DATA", "No such document");
                                         }
+                                    } else {
+                                        Log.d("QUERY_DATA", "query failed with ", task.getException());
                                     }
-                                    else{
-                                        Log.d("READ_DATA", "get failed with ", task.getException());
-                                    }
-                            }});
+                                }
+                            });
                 }
             }
         });
@@ -266,44 +253,28 @@ public class ManageRequestsFragment extends Fragment {
                                                             @Override
                                                             public void onComplete(@NonNull Task<Void> task) {
                                                                 if (task.isSuccessful()) {
-                                                                    db.collection("users2").whereEqualTo("user_info.username", clickedBook.getOwner())
-                                                                            .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                                    docRef.update("my_books." + bookID, "accepted").addOnCompleteListener(new OnCompleteListener<Void>() {
                                                                         @Override
-                                                                        public  void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                                        public void onComplete(@NonNull Task<Void> task) {
                                                                             if (task.isSuccessful()) {
-                                                                                DocumentSnapshot doc = task.getResult().getDocuments().get(0);
-                                                                                if (doc.exists()) {
-                                                                                    db.collection("users2").document(doc.getId()).update("my_books." + bookID, "accepted").addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                                                        @Override
-                                                                                        public void onComplete(@NonNull Task<Void> task) {
-                                                                                            if (task.isSuccessful()) {
-                                                                                                myDataset.remove(clickedBook);
-                                                                                                for (Book book: myDataset){
-                                                                                                    if(book.getBookTitle()==clickedBook.getBookTitle()){
-                                                                                                        myDataset.remove(book);
-                                                                                                        mAdapter.notifyDataSetChanged();
-                                                                                                    }
-                                                                                                }
-                                                                                                mAdapter.notifyDataSetChanged();
-                                                                                                Toast.makeText(getContext(), "Request by: " + clickedBook.getRequester() + " for: " + clickedBook.getBookTitle() + " accepted ", Toast.LENGTH_SHORT).show();
-                                                                                            } else {
-                                                                                                Log.d("UPDATE_DATA", "update failed with ", task.getException());
-                                                                                            }
-                                                                                        }
-                                                                                    });
-                                                                                } else {
-                                                                                    Log.d("READ_DATA", "No such document");
+                                                                                myDataset.remove(clickedBook);
+                                                                                for (Book book : myDataset) {
+                                                                                    if (book.getBookTitle() == clickedBook.getBookTitle()) {
+                                                                                        myDataset.remove(book);
+                                                                                        mAdapter.notifyDataSetChanged();
+                                                                                    }
                                                                                 }
+                                                                                mAdapter.notifyDataSetChanged();
+                                                                                Toast.makeText(getContext(), "Request by: " + clickedBook.getRequester() + " for: " + clickedBook.getBookTitle() + " accepted ", Toast.LENGTH_SHORT).show();
                                                                             } else {
-                                                                                Log.d("QUERY_DATA", "query failed with ", task.getException());
+                                                                                Log.d("UPDATE_DATA", "update failed with ", task.getException());
                                                                             }
                                                                         }});
                                                                     mAdapter.notifyDataSetChanged();
                                                                 } else {
                                                                     Log.d("UPDATE_DATA", "update failed with ", task.getException());
                                                                 }
-                                                            }
-                                                        });
+                                                            }});
                                                         mAdapter.notifyDataSetChanged();
                                                     } else {
                                                         Log.d("UPDATE_DATA", "update failed with ", task.getException());
