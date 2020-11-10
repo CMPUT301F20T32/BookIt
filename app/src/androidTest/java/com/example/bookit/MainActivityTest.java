ecb686d0d49595223461b00d560fb03b4cc9dfe5
Vyome Agarwal <vyome@ualberta.ca>
2020-11-10 14:44:27 -0700

    added Robotium and Tests for MainActivity

---
+++ app/src/androidTest/java/com/example/bookit/MainActivityTest.java
@@ -0,0 +1,41 @@
+package com.example.bookit;
+
+import android.widget.EditText;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MainActivityTest {
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<LoginActivity> rule =
+            new ActivityTestRule<LoginActivity>(LoginActivity.class, true, true);
+
+    @Before
+    public void setUp() {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+
+        // enter the email
+        solo.enterText((EditText) solo.getView(R.id.editTextTextEmailAddress), "sheldon@gmail.com");
+
+        //enter the pass
+        solo.enterText((EditText) solo.getView(R.id.editTextTextPassword), "1234567");
+    }
+
+    @Test
+    public void showMainActivity() {
+        //click on login
+        solo.clickOnButton("Login");
+
+        // Check if MainActivity is launched
+        solo.waitForActivity(MainActivity.class);
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
+    }
+}
