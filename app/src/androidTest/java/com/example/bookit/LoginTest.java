6cd304461c86a82589a2bb7f6a8444d2082312bc
Vyome Agarwal <vyome@ualberta.ca>
2020-11-09 20:37:39 -0700

    added tests for LoginActivity

---
+++ app/src/androidTest/java/com/example/bookit/LoginTest.java
@@ -0,0 +1,56 @@
+package com.example.bookit;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginTest {
+
+    @Rule
+    public ActivityScenarioRule<LoginActivity> mLoginActivityTestRule
+            = new ActivityScenarioRule<>(LoginActivity.class);
+
+    @Before
+    public void setUp() {
+        Intents.init();
+    }
+
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
+
+    @Test
+    public void checkViews() {
+        onView(withId(R.id.editTextTextEmailAddress))
+                .check(matches(isDisplayed()));
+
+        onView(withId(R.id.editTextTextPassword))
+                .check(matches(isDisplayed()));
+
+        onView(withId(R.id.button))
+                .check(matches(isDisplayed()));
+
+        onView(withId(R.id.imageView))
+                .check(matches(isDisplayed()));
+
+        onView(withId(R.id.textView))
+                .check(matches(isDisplayed()));
+
+        onView(withId(R.id.button2))
+                .check(matches(isDisplayed()));
+
+    }
+}
