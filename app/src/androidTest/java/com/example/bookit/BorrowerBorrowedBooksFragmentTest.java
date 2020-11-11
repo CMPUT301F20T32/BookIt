e491cfab8e873f602650be0f0e12155b50ad638a
Vyome Agarwal <vyome@ualberta.ca>
2020-11-11 16:21:34 -0700

    removed an espresso test

--- app/src/androidTest/java/com/example/bookit/BorrowerBorrowedBooksFragmentTest.java
+++ app/src/androidTest/java/com/example/bookit/BorrowerBorrowedBooksFragmentTest.java
@@ -3,7 +3,6 @@ package com.example.bookit;
 import android.view.View;
 import android.widget.EditText;
 
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 
@@ -16,9 +15,8 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertTrue;
 
 /**
  * This class is used for intent testing of the borrowed books of the borrower
@@ -50,17 +48,17 @@ public class BorrowerBorrowedBooksFragmentTest {
         // wait for MainActivity to open
         solo.waitForActivity(MainActivity.class, 20000);
         solo.waitForView(R.id.action_settings);
-        //solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
 
         // check if the bottom bar is visible
-//        assertTrue(solo.searchText("Borrowed"));
-//        assertTrue(solo.searchText("My Requests"));
-//        assertTrue(solo.searchText("Search"));
+        assertTrue(solo.searchText("Borrowed"));
+        assertTrue(solo.searchText("My Requests"));
+        assertTrue(solo.searchText("Search"));
 
         // check if the user is in "borrower" mode
-//        assertTrue(solo.searchText("Borrower"));
+        assertTrue(solo.searchText("Borrower"));
 
-        onView(withId(R.id.borrowed_borrower_recycler_view)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        //onView(withId(R.id.borrowed_borrower_recycler_view)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
 
         // check if action_settings is visible
         //onView(withId(R.id.action_settings)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
