e4da3d887cb3a504c3120a081aa570bdf8795f36
Vyome Agarwal <vyome@ualberta.ca>
2020-11-11 13:25:33 -0700

    added intent tests for accepted requests

--- app/src/androidTest/java/com/example/bookit/MyRequestsTest.java
+++ app/src/androidTest/java/com/example/bookit/MyRequestsTest.java
@@ -15,12 +15,13 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.swipeLeft;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertTrue;
 
-public class BorrowerBorrowedBooksFragmentTest {
+public class MyRequestsTest {
     private Solo solo;
 
     @Rule
@@ -37,16 +38,20 @@ public class BorrowerBorrowedBooksFragmentTest {
         // enter the login information for the user
         solo.enterText((EditText) solo.getView(R.id.editTextTextEmailAddress), "howard@gmail.com");
         solo.enterText((EditText) solo.getView(R.id.editTextTextPassword), "1234567");
-    }
 
-    @Test
-    public void checkBorrowerBorrowedBooksFragment() {
         // click on the login button
         solo.clickOnButton("Login");
 
         // wait for MainActivity to open
         solo.waitForActivity(MainActivity.class);
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+    }
+
+    @Test
+    public void checkMyRequests() {
+
+        // Click on the My Requests Button
+        onView(withId(R.id.MyRequestsFragment)).perform(click());
 
         // check if the bottom bar is visible
         assertTrue(solo.searchText("Borrowed"));
@@ -56,8 +61,15 @@ public class BorrowerBorrowedBooksFragmentTest {
         // check if the user is in "borrower" mode
         assertTrue(solo.searchText("Borrower"));
 
-        onView(withId(R.id.borrowed_borrower_recycler_view)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.my_requests_tab_layout)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.my_requests_pager)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.pending_requests_borrower_recycler_view)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        assertTrue(solo.searchText("Pending"));
+        assertTrue(solo.searchText("Accepted"));
 
+        // swipe left
+        onView(withId(R.id.my_requests_pager)).perform(swipeLeft());
+        onView(withId(R.id.accepted_requests_borrower_recycler_view)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
     }
 
     @After
