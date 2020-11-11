11ddc8ed4a11f65bfb084dd9f35004f6eab63df7
Vyome Agarwal <vyome@ualberta.ca>
2020-11-11 16:58:31 -0700

    changed Before tag to a private method

--- app/src/androidTest/java/com/example/bookit/BorrowerBorrowedBooksFragmentTest.java
+++ app/src/androidTest/java/com/example/bookit/BorrowerBorrowedBooksFragmentTest.java
@@ -2,6 +2,7 @@ package com.example.bookit;
 
 import android.widget.EditText;
 
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 
@@ -13,6 +14,10 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -45,7 +50,7 @@ public class BorrowerBorrowedBooksFragmentTest {
         // wait for MainActivity to open
         solo.waitForActivity(MainActivity.class, 20000);
         solo.waitForView(R.id.action_settings);
-        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        //solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
 
         // check if the bottom bar is visible
         assertTrue(solo.searchText("Borrowed"));
@@ -55,7 +60,7 @@ public class BorrowerBorrowedBooksFragmentTest {
         // check if the user is in "borrower" mode
         assertTrue(solo.searchText("Borrower"));
 
-        //onView(withId(R.id.borrowed_borrower_recycler_view)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.borrowed_borrower_recycler_view)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
 
         // check if action_settings is visible
         //onView(withId(R.id.action_settings)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
--- app/src/androidTest/java/com/example/bookit/MyRequestsTest.java
+++ app/src/androidTest/java/com/example/bookit/MyRequestsTest.java
@@ -1,7 +1,5 @@
 package com.example.bookit;
 
-import android.widget.EditText;
-
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -9,17 +7,18 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import com.google.firebase.auth.FirebaseAuth;
 import com.robotium.solo.Solo;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.swipeLeft;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertTrue;
 
 /**
  * This class is used for intent testing of the requested books by the borrower
@@ -31,59 +30,63 @@ public class MyRequestsTest {
     public ActivityScenarioRule<LoginActivity> rule =
             new ActivityScenarioRule<>(LoginActivity.class);
 
-    @Before
-    public void setUp() {
+    private void setUp() {
 
         rule.getScenario().onActivity(activity -> solo =
                 new Solo(InstrumentationRegistry.getInstrumentation(), activity));
 
 
         // enter the login information for the user
-        solo.enterText((EditText) solo.getView(R.id.editTextTextEmailAddress), "howard@gmail.com");
-        solo.enterText((EditText) solo.getView(R.id.editTextTextPassword), "1234567");
+//        solo.enterText((EditText) solo.getView(R.id.editTextTextEmailAddress), "howard@gmail.com");
+//        solo.enterText((EditText) solo.getView(R.id.editTextTextPassword), "1234567");
+
+        onView(withId(R.id.editTextTextEmailAddress)).perform(typeText("howard@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.editTextTextPassword)).perform(typeText("1234567"), closeSoftKeyboard());
 
         // click on the login button
-        solo.clickOnButton("Login");
+        //solo.clickOnButton("Login");
+
+        onView(withId(R.id.button)).perform(click());
 
         // wait for MainActivity to open
         solo.waitForActivity(MainActivity.class, 20000);
-        solo.waitForView(R.id.action_settings);
+//        solo.waitForView(R.id.action_settings);
         //solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
     }
 
     @Test
     public void checkMyRequests() {
+        setUp();
 
         // Click on the My Requests Button
         onView(withId(R.id.MyRequestsFragment)).perform(click());
 
         // check if the bottom bar is visible
-//        assertTrue(solo.searchText("Borrowed"));
-//        assertTrue(solo.searchText("My Requests"));
-//        assertTrue(solo.searchText("Search"));
+        assertTrue(solo.searchText("Borrowed"));
+        assertTrue(solo.searchText("My Requests"));
+        assertTrue(solo.searchText("Search"));
 
         // check if the user is in "borrower" mode
-//        assertTrue(solo.searchText("Borrower"));
+        assertTrue(solo.searchText("Borrower"));
 
         solo.waitForView(R.id.my_requests_tab_layout);
         onView(withId(R.id.my_requests_tab_layout)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
         onView(withId(R.id.my_requests_pager)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
         onView(withId(R.id.pending_requests_borrower_recycler_view)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
-//        assertTrue(solo.searchText("Pending"));
-//        assertTrue(solo.searchText("Accepted"));
+        assertTrue(solo.searchText("Pending"));
+        assertTrue(solo.searchText("Accepted"));
 
         // swipe left
         onView(withId(R.id.my_requests_pager)).perform(swipeLeft());
         onView(withId(R.id.accepted_requests_borrower_recycler_view)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+        tearDown();
     }
 
-    @After
-    public void tearDown() {
+    private void tearDown() {
 
         // log the user out
         FirebaseAuth.getInstance().signOut();
-
-        solo.finishOpenedActivities();
 
     }
 }
