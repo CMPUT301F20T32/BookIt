558f1275c4c3affeef59d01262b622d2386ad2b1
Vyome Agarwal <vyome@ualberta.ca>
2020-11-28 19:17:22 -0700

    added javadoc

--- app/src/main/java/com/example/bookit/BookInfoFragment.java
+++ app/src/main/java/com/example/bookit/BookInfoFragment.java
@@ -196,6 +196,16 @@ public class BookInfoFragment extends Fragment {
         });
     }
 
+    /**
+     * Called when an activity you launched exits, giving you the requestCode you started it with,
+     * the resultCode it returned, and any additional data from it
+     *
+     * @param requestCode The integer request code originally supplied to startActivityForResult(),
+     *                    allowing you to identify who this result came from.
+     * @param resultCode  The integer result code returned by the child activity through its setResult()
+     * @param data        An Intent, which can return result data to the caller
+     *                    (various data can be attached to Intent "extras").
+     */
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1) {
@@ -244,6 +254,9 @@ public class BookInfoFragment extends Fragment {
         }
     }
 
+    /**
+     * Queries Firestore to assign the DocumentId of a book to docId
+     */
     private void assignBookID() {
         db.collection("books")
                 .document(getArguments().getString("bookId")).get()
